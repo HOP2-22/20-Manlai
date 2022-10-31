c4901373be767b88bb05010106ed7cc482b3b0da
Manlai <manlainest@gmail.com>
2022-10-31 18:16:15 +0800

    day10

--- block1/day7/src/App.js
+++ block1/day7/src/App.js
@@ -1,16 +1,38 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  const timeShower = {
+    timeText: {
+      fontWeight: "bold",
+      fontSize: "100px",
+      color: "white",
+    },
+  };
+
+  useEffect(() => {
+    let interval = undefined;
+
+    if (start) {
+      interval = setInterval(() => {
+        setTime((pre) => pre + 1);
+      }, 10);
+    }
+
+    return () => clearInterval(interval);
+  }, [start]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path=":username" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ background: "black" }}>
+      <div style={timeShower.timeText}>
+        {Math.floor(time / 6000)}:{Math.floor(time / 100) % 60}:{time % 100}
+      </div>
+      <button onClick={() => setStart(true)}>start</button>
+      <button onClick={() => setStart(false)}>stop</button>
+      <button onClick={() => setTime(0)}>reset</button>
+    </div>
   );
 }
 
