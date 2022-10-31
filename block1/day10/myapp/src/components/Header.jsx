2da756412a5e07739c8566f7bb9fc628d1e34b8e
Manlainest <manlainest@gmail.com>
2022-10-31 19:22:35 +0800

    a

---
+++ block1/day10/myapp/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+export const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <div className="l-nav">team.</div>
+      </div>
+      <div className="r-nav">
+        <a href="">Products</a>
+        <a href="">Services</a>
+        <a href="">Contact</a>
+        <a href="">Log in</a>
+        <a href="">Get access</a>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- block1/day5/src/App.js
+++ block1/day5/src/App.js
@@ -1,35 +1,72 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
+import users from "./users.json";
 
-// const styles = {
-//   userCardContainer: {};
-// }
+const styles = {
+  userCardContainer: {
+    display: "flex",
+    border: "1px solid black",
+    borderRadius: "8px",
+    margin: "12px",
+    width: "400px",
+  },
+  searchInput: {
+    height: "32px",
+    width: "400px",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
 
-function App() {
-  const [outPut, setOutPut] = useState(users);
+const UserCard = ({ userData }) => {
+  return (
+    <div style={styles.userCardContainer}>
+      <img
+        src={userData.avatar}
+        width={100}
+        height={100}
+        alt={userData.username}
+      />
+      <div>
+        <p>UserName: {userData.username}</p>
+        <p>Email: {userData.email}</p>
+        <p>Birth: {userData.date_of_birth}</p>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [output, setOutput] = useState(users);
   const search = (val) => {
-    setOutPut(() => {
-      return user.username.toLowerCase().includes(val.toLowerCase());
-    });
+    setOutput(
+      users.filter((user) => {
+        return user.username.toLowerCase().includes(val.toLowerCase());
+      })
+    );
   };
   return (
-    <div styles={styles.container}>
+    <div style={styles.container}>
       <div>
         <input
-          type="text"
-          onchange={(e) => {
+          type={"text"}
+          onChange={(e) => {
             search(e.target.value);
           }}
           style={styles.searchInput}
         />
       </div>
       {output.length === 0 ? (
-        <div>No user found.</div>
-        ) : (
-          output.map((userData, index) => {
-            return <UserCard userdata={userData} key={index} />;
+        <div>No User Found</div>
+      ) : (
+        output.map((userData, index) => {
+          return <UserCard userData={userData} key={index} />;
         })
-        )
-        </div>
-      }
+      )}
+    </div>
+  );
+};
 
 export default App;
